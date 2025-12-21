98c2ac052d303815e8ea3193d80cc9a83662a1a5
Amit K. Prasad <kus8347052@gmail.com>
2025-12-21 20:46:34 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:5173"],
+  origin: ["http://localhost:3000", "http://localhost:5173", process.env.FRONTEND_URL],
   credentials: true
 }));
 
